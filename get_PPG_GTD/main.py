f1cc2a60577cdbd7182d66e77997a6ac4f4813e0
jarvis.geun <jarvis.geun@gmaill.com>
2022-09-30 19:16:49 +0900

    add args of path and name

--- get_PPG_GTD/main.py
+++ get_PPG_GTD/main.py
@@ -5,6 +5,7 @@ import threading
 import argparse
 import numpy as np
 import serial
+import os
 
 from camera.JetsonCamera import Camera
 from camera.Focuser import Focuser
@@ -21,12 +22,27 @@ signal.signal(signal.SIGTERM, sigint_handler)
 def parse_cmdline():
     parser = argparse.ArgumentParser(description='Arducam IMX519 Autofocus Demo.')
 
-    parser.add_argument('-i', '--i2c-bus', type=int, nargs=None, required=True,
+    parser.add_argument('-i', '--i2c-bus', type=int, nargs=None, required=True, default=7,
                         help='Set i2c bus, for A02 is 6, for B01 is 7 or 8, for Jetson Xavier NX it is 9 and 10.')
 
     parser.add_argument('-v', '--verbose', action="store_true", help='Print debug info.')
+    parser.add_argument('-p', '--path', type=str, required=True,
+                        help='path of rgb and ppg data')
+    parser.add_argument('-n', '--name', type=str, required=True,
+                        help='name of video and ppg')
+    parser.add_argument('-t', '--time', type=int, required=True,
+                        help='time of video')
 
     return parser.parse_args()
+
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory ' + directory)
+
 
 if __name__ == "__main__":
     py_serial = serial.Serial(
@@ -44,24 +60,33 @@ if __name__ == "__main__":
     focusState.verbose = args.verbose
     doFocus(camera, focuser, focusState)
 
+    path = args.path
+    name = args.name
+    len_time = args.time
+
+    createFolder(path)
+
     w = 640
     h = 360
     fps = 25
     fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-    out = cv2.VideoWriter("output.avi", fourcc, fps, (w, h))
+    out = cv2.VideoWriter("{}/{}.avi".format(path, name), fourcc, fps, (w, h))
 
+    # for fps
     start = time.time()
-    start_time = time.time()
     frame_count = 0
 
+    # for elapsed time
+    start_time = time.time()
+
     i = 0
-    f = open("ppg_data.txt", 'w')
+    f = open("{}/{}.txt".format(path, name), 'w')
 
     while not exit_:
-        print("elapsed time : ", time.time()-start_time)
+        print("elapsed time : {} seconds".format(time.time() - start_time))
 
-        if time.time() - start_time > 60:
-            print("60 seconds end")
+        if time.time() - start_time > len_time:
+            print("{} seconds end".format(len_time))
             break
         frame = camera.getFrame(2000)
 
@@ -69,10 +94,10 @@ if __name__ == "__main__":
             ppg = py_serial.readline()
             f.write(str(i) + ' ' + str(ppg) + '\n')
 
-            cv2.imshow("Test", frame)
+            cv2.imshow("frame", frame)
             out.write(frame)
             i += 1
-            print("i : ", i)
+            print("frame count : ", i)
 
         key = cv2.waitKey(1)
         if key == ord('q'):
