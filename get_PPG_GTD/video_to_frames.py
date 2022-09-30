e7eec2d748b97a8167c05286639037530f863297
jarvis.geun <jarvis.geun@gmaill.com>
2022-09-30 23:16:32 +0900

    add arguparse

--- get_PPG_GTD/video_to_frames.py
+++ get_PPG_GTD/video_to_frames.py
@@ -1,14 +1,33 @@
 import cv2
+import argparse
+import os
 
-cap = cv2.VideoCapture("output.avi")
+
+parser = argparse.ArgumentParser(description='Video to frames')
+parser.add_argument('-n', '--name', type=str, required=True,
+                    help='name of RGB video')
+args = parser.parse_args()
+name = args.name
+
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory ' + directory)
+
+createFolder(name)
+
+cap = cv2.VideoCapture("{}.avi".format(name))
 frame_cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 print("frame count : ", frame_cnt)
 
 i = 0
 while(cap.isOpened()):
     ret, frame = cap.read()
-    cv2.imshow("frame", frame)
-    cv2.imwrite("frames/{}.png".format(i), frame)
+    cv2.imshow("{}".format(name), frame)
+    cv2.imwrite("{}/{}.png".format(name, i), frame)
     print("{}.png".format(i))
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
